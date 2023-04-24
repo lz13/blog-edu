ccb899784446fa0429cb7040bdfa8a832f1b38c9
Lovro Zelić <lovro.zelic@icloud.com>
2023-04-24 11:50:16 +0200

    add a Blog post Show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # GET /blog_posts/1
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  
+
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
