7f6a7b53c696c13f699ce23694ecddbb20ed5d65
Lovro Zelić <lovro.zelic@icloud.com>
2023-04-24 15:45:22 +0200

    Add Edit & Update Blog Post Actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   # GET /blog_posts/new
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-
   # GET /blog_posts/1
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-
+  # PATCH/PUT /blog_posts/1
+  patch "/blog_posts/:id", to: "blog_posts#update"
+  # GET /blog_posts/1/edit
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   # POST /blog_posts
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   
