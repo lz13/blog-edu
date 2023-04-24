182d9fb2f3696f67359b12334833c2469716186f
Lovro Zelić <lovro.zelic@icloud.com>
2023-04-24 16:33:12 +0200

    Add authentification woth Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  devise_for :users
   
   # GET /blog_posts/new
   # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create a default user
+user = User.where(email: "udoyorgan@gmail.com").first_or_initialize
+user.update!(
+  password: "password",
+  password_confirmation: "password"
+)
