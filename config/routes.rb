bbb410eee12d186d120d7c267e7d2210ae421121
Lovro Zelić <lovro.zelic@icloud.com>
2023-04-24 16:01:51 +0200

    refactor routes and blog_posts_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # GET /blog_posts/new
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  # GET /blog_posts/1
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-  # PATCH/PUT /blog_posts/1
-  patch "/blog_posts/:id", to: "blog_posts#update"
-  # DELETE /blog_posts/1
-  delete "/blog_posts/:id", to: "blog_posts#destroy"
-  # GET /blog_posts/1/edit
-  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
-  # POST /blog_posts
-  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
   
+  # GET /blog_posts/new
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # GET /blog_posts/1
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # PATCH/PUT /blog_posts/1
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # DELETE /blog_posts/1
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # GET /blog_posts/1/edit
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # POST /blog_posts
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+  
+  resources :blog_posts # shortcut for routes above
 
 
   # Defines the root path route ("/")
