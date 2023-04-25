c5c6a448f1d864a523e36846021a2222e5922d45
Lovro Zelić <lovro.zelic@icloud.com>
2023-04-25 12:32:05 +0200

    Add published_at for draft, published, and scheduled blog posts

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,37 @@
 class BlogPost < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where("published_at <= ?", Time.current) }
+  scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+  def draft?
+    published_at.nil?
+  end
+
+  def published?
+    published_at? && published_at <= Time.current
+  end
+
+  def scheduled?
+    published_at? && published_at > Time.current
+  end
 end
+
+# `status` string field
+# - draft
+# - published
+# - scheduled
+
+# `published_at` datetime field
+# - nil
+# - 1.year.ago
+# - 1.year.from_now
+
+# BlogPost.all
+# BlogPost.draft
+# BlogPost.published
+# BlogPost.scheduled
+
